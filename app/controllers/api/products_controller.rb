75c68d15cd1d64b60cb7b1120a6a5db110f6bb52
stephenhofstad <stevehofstad@Steves-MacBook-Air.local>
2020-03-03 19:00:24 -0600

    added info to the database and created a route and controller to display it

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def foto_seller
+    @photo = Product.all
+    render "feetfoto.json.jb"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 photo1 = Product.new({ shoe_size: "7", polish_color: "red", price: "20.00", description: "polaroid of one foot stepping in chocolate cake", gender: "female" })
+photo1.save
 photo2 = Product.new({ shoe_size: "24", polish_color: "clear", price: "200.00", description: "polaroid of shaquille o'neal getting a pedicure", gender: "male" })
+photo2.save
 photo3 = Product.new({ shoe_size: "5.5", polish_color: "green", price: "10.00", description: "oil painting of ballerina on tip toes", gender: "female" })
+photo3.save
